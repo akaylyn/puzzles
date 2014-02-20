5ab8ac605a6d5fd693a304dddbc90e2d97f2333d
Andrea Pollitt <andrea.pollitt@gmail.com>
2014-02-19 22:38:38 -0500

    create a stack with the ability to retrieve the min value

---
+++ 002-StackWithMin.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Node structure:
+ *
+ * Each node has a pointer to the min value at the time it was
+ * added to the stack.
+ */
+typedef struct Node {
+    int value;
+    struct Node *prev;
+    struct Node *min;
+} Node;
+
+/**
+ * Stack structure:
+ *
+ * The stack keeps a pointer to the top.
+ */
+typedef struct Stack {
+    Node *top;
+} Stack;
+
+void printStack(Node *curr);
+void push(Stack *stack, Node *node);
+void pop(Stack *stack);
+int min(Stack *stack);
+Node * createNode(int value);
+Stack * createStack();
+
+int main()
+{
+    Stack *stack = createStack();
+    Node *a = createNode(15);
+    Node *b = createNode(3);
+    Node *c = createNode(2);
+    Node *d = createNode(12);
+
+    push(stack, a);
+    push(stack, b);
+    push(stack, c);
+    push(stack, d);
+
+    pop(stack);
+
+    printStack(stack->top);
+    printf("The minimum value in the stack is: %d\n", min(stack));
+
+    return 0;
+}
+
+void printStack(Node *curr)
+{
+    if (curr == NULL) {
+        return;
+    }
+
+    printf("%d, curr min: %d\n", curr->value, curr->min->value);
+    printStack(curr->prev);
+}
+
+void push(Stack *stack, Node *node)
+{
+    node->prev = stack->top;
+    stack->top = node;
+
+    if (node->prev == NULL) {
+        return;
+    }
+
+    // determine if the newest node is the new min node
+    // in the future, consider doing this above for less de-referencing
+    if (node->value < node->prev->min->value) {
+        node->min = node;
+    } else {
+        node->min = node->prev->min;
+    }
+}
+
+void pop(Stack *stack)
+{
+    stack->top = stack->top->prev;
+}
+
+int min(Stack *stack)
+{
+    return stack->top->min->value;
+}
+
+Node * createNode(int value)
+{
+   Node *newNode = (Node *)malloc(sizeof(Node));
+
+   if (newNode == NULL) {
+       printf("Error creating new node.\n");
+       exit(0);
+   }
+
+   newNode->min = newNode;
+   newNode->value = value;
+   return newNode;
+}
+
+Stack * createStack()
+{
+    Stack *newStack = (Stack *)malloc(sizeof(Stack));
+
+    if (newStack == NULL) {
+        printf("Error creating new stack.\n");
+        exit(0);
+    }
+
+    return newStack;
+}
+
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ You have two numbers represented by a linked list, where each node contains a si
 ## 001-AddLinkedListsReverse.c
 
 Same as 000, only the list is reversed.
+
+## 002-StackWithMin.c
+
+How would you design a stack which, in addition to push and pop, also has a function min which returns the minimum element? Push, pop, and min should all operate in O(1) time.
